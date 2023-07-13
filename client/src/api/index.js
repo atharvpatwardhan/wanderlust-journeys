e5a2e203e227a36fd943e996c86cb16ff36e8356
Atharv Patwardhan <84259526+atharvpatwardhan@users.noreply.github.com>
2023-07-13 21:57:37 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const url = 'http://localhost:5000/posts';
 
-const API = axios.create({baseURL:'http://localhost:5000'});
+const API = axios.create({baseURL:'https://wanderlust-journeys-backend-git-main-atharvpatwardhan.vercel.app'});
 
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile')){
