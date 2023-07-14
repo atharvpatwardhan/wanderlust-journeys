614b33d9c93a57580f8ebdd7b491f5ff5d3ad7f2
Atharv Patwardhan <84259526+atharvpatwardhan@users.noreply.github.com>
2023-07-14 15:26:05 +0530

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,9 +5,9 @@ export const getPosts = async (req,res) =>{
     try{
         const postMessages = await PostMessage.find();
         // console.log(postMessages);
+        res.header("Access-Control-Allow-Origin", "*");
 
         res.status(200).json(postMessages);
-        res.header("Access-Control-Allow-Origin", "*");
     }
     catch(error){
         res.status(404).json({message:error.message});
