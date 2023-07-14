6bb13ba5f44e95f383ed5d310818779639e8f6fc
Atharv Patwardhan <84259526+atharvpatwardhan@users.noreply.github.com>
2023-07-14 14:06:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(bodyParser.json({limit:"30mb",extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended: true}));
-app.use(cors());
+app.use(cors({
+  origin: ["wanderlust-journeys-frontend.vercel.app"],
+  methods: ["POST","GET","PUT","DELETE"],
+  credentials: true
+}));
 
 
 app.use('/posts',postRoutes);
@@ -20,5 +24,3 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect("mongodb+srv://atharv:atharv2611@cluster0.9gmrfb8.mongodb.net/",{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>app.listen(PORT,()=>console.log(`Server running on port : ${PORT}`)))
 .catch((error)=>console.log(error.message));
-
-// module.exports = app;
