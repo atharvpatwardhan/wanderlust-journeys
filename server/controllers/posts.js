5e245ebb08cb8ec3f89224f5d77d025214ad4ab0
Atharv Patwardhan <84259526+atharvpatwardhan@users.noreply.github.com>
2023-07-14 15:46:11 +0530

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,15 +2,17 @@ import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req,res) =>{
+
+    res.header("Access-Control-Allow-Origin", "*");
+
     try{
         const postMessages = await PostMessage.find();
         // console.log(postMessages);
-        res.header("Access-Control-Allow-Origin", "*");
-
         res.status(200).json(postMessages);
+
     }
     catch(error){
-        res.status(404).json({message:error.message});
+        res.status(404).json({message:error.message});    
     }
 }
 
@@ -20,10 +22,13 @@ export const getPost = async (req,res) =>{
 
     try{
         const post = await PostMessage.findById(id);
+        res.header("Access-Control-Allow-Origin", "*");
 
         res.status(200).json(post);
     }
     catch(error){
+        res.header("Access-Control-Allow-Origin", "*");
+
         res.status(404).json({message:error.message});
 
     }
@@ -39,10 +44,14 @@ export const getPostsBySearch = async(req,res) => {
         const title = new RegExp(searchQuery,'i');
         const posts = await PostMessage.find({title});
         // console.log(posts);
+        res.header("Access-Control-Allow-Origin", "*");
+
         res.json({data: posts});
         
     } 
     catch (error) {
+        res.header("Access-Control-Allow-Origin", "*");
+
         res.status(404).json({message:error})
     }
 }
@@ -51,11 +60,13 @@ export const getPostsBySearch = async(req,res) => {
 
 export const createPost = async (req,res) =>{
     const post = req.body;
+    res.header("Access-Control-Allow-Origin", "*");
 
     const newPost = new PostMessage({ ...post,creator : req.userId, createdAt : new Date().toISOString() });
 
     try{
         await newPost.save();
+        
 
         res.status(201).json(newPost);
     }
@@ -68,6 +79,9 @@ export const updatePost = async(req,res) => {
     const { id : _id } = req.params;
     const post = req.body;
 
+    res.header("Access-Control-Allow-Origin", "*");
+
+
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No Post with given Id.');
 
     const updatedPost = await PostMessage.findByIdAndUpdate(_id,post,{new:true});
@@ -79,12 +93,14 @@ export const deletePost = async(req,res) => {
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Post with given Id.');
 
     await PostMessage.findByIdAndRemove(id);
+    res.header("Access-Control-Allow-Origin", "*");
 
     res.json({message:'Post Deleted.'});
 }
 
 export const likePost = async(req,res) => {
     const { id } = req.params;
+    res.header("Access-Control-Allow-Origin", "*");
 
     if(!req.userId) return res.json({message:'Unauthenticated User.'});
 
@@ -111,7 +127,9 @@ export const commentPost = async(req,res) => {
 
     const { value } = req.body;
 
-    console.log(value);
+    // console.log(value);
+    res.header("Access-Control-Allow-Origin", "*");
+
 
     const post = await PostMessage.findById(id);
 
